a4ccf1ef9d1587f2fcb5fa7cf71d5ca9ece06a5f
shivani patel <shivani.patel@r-one.in>
2024-05-17 11:24:27 +0530

    some changes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
     def index
-        debugger
+        
         @categories = Category.page(params[:page]).per(params[:per_page] || 4)
         render json: @categories
 
